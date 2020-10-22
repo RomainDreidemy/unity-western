cfe942b8e43640cae98f47b64b26560fe8059c8b
Romain Dreidemy <dreidemyromain@gmail.com>
2020-10-22 19:27:18 +0200

    Add level mode

---
+++ Assets/Script/Global.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Global : MonoBehaviour
+{
+    public static int DIFICULTY;
+    public static int EASY = 0;
+    public static int MEDIUM = 1;
+    public static int HARDCORE = 2;
+
+    public void SetDificulty(int dif = 0)
+    {
+        Global.DIFICULTY = dif;
+        SceneManager.LoadScene("Game");
+    }
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Script/Life.cs
+++ Assets/Script/Life.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class Life : MonoBehaviour
 {
     public int PlayerLife = 5;
+    int PlayerMaxLife = 5;
     public UnityEngine.UI.Image PictureLife1;
     public UnityEngine.UI.Image PictureLife2;
     public UnityEngine.UI.Image PictureLife3;
     public UnityEngine.UI.Image PictureLife4;
     public UnityEngine.UI.Image PictureLife5;
 
+    Global globalRef;
+
+    void Start()
+    {
+        this.globalRef = GetComponent<Global>();
+        this.initLife();
+        Debug.Log(this.PlayerMaxLife);
+    }
+
     public void addPlayerLife(int Value)
     {
-        if(this.PlayerLife < 5)
+        if(this.PlayerLife < this.PlayerMaxLife)
         {
             this.PlayerLife += Value;
         }
@@ -57,5 +67,40 @@ public class Life : MonoBehaviour
     public void updateLifeboard()
     {
         //TODO : update l'affichage des vies
+    }
+
+    void initLife()
+    {
+        switch (Global.DIFICULTY)
+        {
+            case 0 :
+                this.PlayerLife = this.PlayerMaxLife = 5;
+                this.PictureLife1.enabled = true;
+                this.PictureLife2.enabled = true;
+                this.PictureLife3.enabled = true;
+                this.PictureLife4.enabled = true;
+                this.PictureLife5.enabled = true;
+            break;
+            case 1 : 
+                this.PlayerLife = this.PlayerMaxLife = 3;
+                this.PictureLife1.enabled = true;
+                this.PictureLife2.enabled = true;
+                this.PictureLife3.enabled = true;
+                this.PictureLife4.enabled = false;
+                this.PictureLife5.enabled = false;
+            break;
+            case 2 : 
+                this.PlayerLife = this.PlayerMaxLife = 1;
+                this.PictureLife1.enabled = true;
+                this.PictureLife2.enabled = false;
+                this.PictureLife3.enabled = false;
+                this.PictureLife4.enabled = false;
+                this.PictureLife5.enabled = false;
+            break;
+            
+            default:
+                this.PlayerLife = 5;
+            break;
+        }
     }
 }
