1a9fe3054f1f0c990fab092ca991fcf97b856751
Romain Dreidemy <dreidemyromain@gmail.com>
2020-10-22 18:06:35 +0200

    See dynamic score

--- Assets/Script/Score.cs
+++ Assets/Script/Score.cs
@@ -1,24 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
 
     public int totalScore = 0;
 
+    public Text TxtScore;
+
+    void Start()
+    {
+        this.updateScoreboard();
+    }
+
     public void AddScore(int Value)
     {
         this.totalScore += Value;
+        this.updateScoreboard();
     }
 
     public void RemoveScore(int Value)
     {
         this.totalScore -= Value;
+        this.updateScoreboard();
     }
 
     void updateScoreboard()
     {
         //TODO : Update le scoreboard
+        this.TxtScore.text = this.totalScore.ToString();
     }
 }
